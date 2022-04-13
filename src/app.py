fa8c4de8a744800eba9399ac1f8e19b92821803d
Jimmy_Tempest <smartwattana@gmail.com>
2022-04-13 19:37:50 +0700

    feat: update to latest template

--- src/app.py
+++ src/app.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
+
+# from starlette.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import uvicorn
+import os
+
+from mysk_utils.response import InternalCode
+
+load_dotenv()
 
 app = FastAPI()
 
 
-
 @app.get("/")
 def healthCheck():
-    return {"status": True}
+    return {"status": True, "internalCode": InternalCode.IC_GENERIC_SUCCESS}
 
+
+if __name__ == "__main__":
+    uvicorn.run(app, host=os.environ.get("HOST"), port=int(os.environ.get("PORT")))
