da995fd9d61ad81d0f5fdc6918e818d51502c0fb
Jimmy_Tempest <smartwattana@gmail.com>
2022-04-22 23:14:31 +0700

    feat: teacher crud get

---
+++ src/db/curd/teacher.py
@@ -0,0 +1,29 @@
+# local modules
+from ..database import engine, teacher_table, people_table
+from db.curd.people import get_person_contact
+
+# internal modules
+from mysk_utils.schema import Teacher
+
+# external modules
+from sqlalchemy import select
+
+
+def get_teacher_by_id(id: int) -> Teacher:
+    """
+    Get teacher by id
+    """
+    conn = engine.connect()
+    query = conn.execute(
+        select([teacher_table, people_table])
+        .where(teacher_table.c.id == id)
+        .join(people_table, teacher_table.c.person_id == people_table.c.id)
+    ).fetchone()
+
+    if query is None:
+        return None
+    # print(dict(query))
+    teacher = Teacher(**dict(query), teacher_id=query["teacher_id"])
+    teacher.contact = get_person_contact(query["person_id"])
+    conn.close()
+    return teacher
